70c181e2922f34c31a1442975d6bce836be0ddd4
Paulo Henrique da Silva Fernandes <paulohenriquedqc@gmail.com>
2019-05-25 15:06:59 -0300

    Injeção de Dependência

---
+++ injecao/DependencyInjection.WeatherSite/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using DependencyInjection.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.WeatherSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ForecastReader _forecastReader;
+
+        public HomeController(ForecastReader forecastReader)
+        {
+            _forecastReader = forecastReader ?? throw new ArgumentNullException(nameof(forecastReader));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetWeather(string query)
+        {
+            var result = await _forecastReader.ReadAsync(query);
+            return View("Index", result);
+        }
+    }
+}
