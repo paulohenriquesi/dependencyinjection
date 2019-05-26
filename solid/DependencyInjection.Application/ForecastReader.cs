3e99ca373284f9c979b28a41dd323e9a03a78207
Paulo Henrique da Silva Fernandes <paulohenriquedqc@gmail.com>
2019-05-26 16:28:01 -0300

    Solid e Design Patterns

---
+++ solid/DependencyInjection.Application/ForecastReader.cs
@@ -0,0 +1,40 @@
+﻿using DependencyInjection.Application.Models;
+using DependencyInjection.Infrastructure;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace DependencyInjection.Application
+{
+    public class ForecastReader : IForecastReader
+    {
+        private WeatherApiStore _weatherApiStore;
+
+
+        public ForecastReader(WeatherApiStore weatherApiStore)
+        {
+            _weatherApiStore = weatherApiStore ?? throw new ArgumentNullException(nameof(weatherApiStore));
+        }
+
+        public async Task<CityForecast> ReadAsync(string query)
+        {
+            var result = await _weatherApiStore.GetAsync(query);
+
+            return new CityForecast
+            {
+                CityId = result.City.Id,
+                CityName = result.City.Name,
+                Forecasts = result.List.Select(x =>
+                    new Forecast
+                    {
+                        Date = DateTime.Parse(x.DtTxt),
+                        MaxTemperature = x.Main.TempMax,
+                        MinTemperature = x.Main.TempMin,
+                        WeatherCondition = x.Weather[0].Description
+                    }).ToList()
+            };
+        }
+    }
+}
---
+++ solid/DependencyInjection.WeatherSite/ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿using DependencyInjection.Application;
+using DependencyInjection.Infrastructure;
+using Refit;
+
+namespace DependencyInjection.WeatherSite.ServiceLocator
+{
+    public class ServiceLocator
+    {
+        public static IOpenWeatherApiClient GetOpenWeatherApiClient()
+        {
+            return RestService.For<IOpenWeatherApiClient>("https://api.openweathermap.org/data/2.5");
+        }
+
+        public static WeatherApiStore GetWeatherApiStore()
+        {
+            return new WeatherApiStore(GetOpenWeatherApiClient());
+        }
+    }
+}
