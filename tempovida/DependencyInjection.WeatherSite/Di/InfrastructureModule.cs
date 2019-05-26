d05b9cc6222a880ad5eda31c94e1a5467ee11cd3
Paulo Henrique da Silva Fernandes <paulohenriquedqc@gmail.com>
2019-05-26 18:29:55 -0300

    Life Time

---
+++ tempovida/DependencyInjection.WeatherSite/Di/InfrastructureModule.cs
@@ -0,0 +1,17 @@
+﻿using DependencyInjection.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+
+namespace DependencyInjection.WeatherSite.Di
+{
+    public static class InfrastructureModule
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+
+            services.AddTransient<IWeatherApiStore, WeatherApiStore>();
+            services.AddSingleton(
+                x => RestService.For<IOpenWeatherApiClient>("https://api.openweathermap.org/data/2.5"));
+        }
+    }
+}
